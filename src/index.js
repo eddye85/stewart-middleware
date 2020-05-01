9af1b857cc2591e6ee7b385d2a3c1ac6cfc77938
Edvin <edvine@gmail.com>
2020-05-01 14:26:51 +0200

    Small fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const Stewart = require("stewart")
 const Quaternion = require('quaternion');
 
+var rad = Math.PI / 180
+
 const platform = new Stewart
-platform.initHexagonal();
+platform.initHexagonal({rodLength:150, hornLength:40})
 
-platform.update([0, 0, 0], Quaternion.ONE);
+platform.update([0, 0, 39], Quaternion.ONE);
 
-console.log(platform.getServoAngles())
+const unfixedAngles = platform.getServoAngles()
+
+const fixedAngles = unfixedAngles.map(value => value ? value * 100 : value)
+
+console.log(fixedAngles)
+//console.log(platform)
